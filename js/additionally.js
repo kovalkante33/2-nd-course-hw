4236ea8d3201bd9eb6226e84272cb27d5ea1cb87
BaluevAG <kovalkante@mail.ru>
2025-06-19 19:51:56 +0300

    решение доп задания по JS

---
+++ js/additionally.js
@@ -0,0 +1,10 @@
+let number = prompt('загадай любое число: ');
+let prod = number * 2;
+alert(`удвоено на два: ${prod}`);
+let sum = prod + 10;
+alert(`прибавили десяток: ${sum}`);
+let divi = sum / 2;
+alert(`пополам: ${divi}`);
+let result = divi - number;
+alert(`результат: ${result}`);
+alert(`ответ равен 5`)
